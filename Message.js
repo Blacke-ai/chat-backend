40ae444b8ac598363f6d8a825215544618ca74e2
Blacke-ai <kosae999r@gmail.com>
2025-10-03 13:54:24 +0300

    Update Message.js

--- Message.js
+++ Message.js
@@ -1,5 +1,10 @@
-require('mongoose');
+const mongoose = require('mongoose');
 
-const messageSchema = new mongoose.Schema({ from: String, to: String, text: String, createdAt: { type: Date, default: Date.now } });
+const messageSchema = new mongoose.Schema({
+  from: String,
+  to: String,
+  text: String,
+  createdAt: { type: Date, default: Date.now }
+});
 
 module.exports = mongoose.model('Message', messageSchema);
